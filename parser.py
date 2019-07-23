0e29ae097b9141c6ed9aa79ea88dec6e271ea14c
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-07-23 18:25:43 +0900

    .

--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 import requests
 import urllib
+import datetime
+import re
 from lxml.html import fromstring
 
-
-xpass = "//*[@id=\"route01\"]/div[5]/div[2]/div/ul/li[3]/ul/li/dl/dd"
+# 各Xpath
 cpass = "//*[@id=\"route01\"]/dl/dd[1]/ul/li[3]"
-#//*[@id="route01"]/dl/dd[1]/ul/li[3]/span[2]
+tpass = "//*[@id=\"route01\"]/div[5]/div[2]/div/ul/li[3]/ul/li/dl/dt"
 
 class ResultData:
     stations:[str]
@@ -21,22 +22,35 @@ def search(from_station:str, to_station:str) -> ResultData:
     sta = urllib.parse.quote(from_station)
     end = urllib.parse.quote(to_station)
 
-    # URL取得
-    content = requests.get(f'https://transit.yahoo.co.jp/search/result?flatlon=&fromgid=&from={sta}&tlatlon=&togid=&to={end}&viacode=&via=&viacode=&via=&viacode=&via=&y=2019&m=07&d=22&hh=16&m2=4&m1=0&type=1&ticket=ic&expkind=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&kw={end}#route01').content
+    #現在時刻取得
+    now = datetime.datetime.now()
 
-    # 駅の配列を作成
-    sta_list = [station.text_content().replace("○", "") for station in fromstring(content).xpath(xpass)]
+    # URL取得
+    content = requests.get(f'https://transit.yahoo.co.jp/search/result?flatlon=&fromgid=&from={sta}&tlatlon=&togid=&to={end}&viacode=&via=&viacode=&via=&viacode=&via=&y={now.year}&m={str(now.month).zfill(2)}&d={str(now.day).zfill(2)}&hh={str(now.hour).zfill(2)}&m2={str(now.minute)[1:]}&m1={str(now.minute).zfill(2)[:1]}&type=1&ticket=ic&expkind=1&ws=3&s=0&shin=1&lb=1&kw={end}#route01').content
+
+    # 駅の配列を生成
+    sta_list = []
+    list_len = [sta1.text_content() for sta1 in fromstring(content).xpath("//*[@id=\"route01\"]/div[5]/div[2]/div")]
+    for i in range(len(list_len)+1):
+        if i % 2 == 0:
+            sta_list += [station.text_content().replace("○", "") for station in fromstring(content).xpath(f"//*[@id=\"route01\"]/div[5]/div[2]/div[{i}]/dl/dt/a")]
+        else:
+            sta_list += [station.text_content().replace("○", "") for station in fromstring(content).xpath(f"//*[@id=\"route01\"]/div[5]/div[2]/div[{i}]/ul/li[3]/ul/li/dl/dd")]
+            sta_list += [station.text_content().replace("○", "") for station in fromstring(content).xpath(f"//*[@id=\"route01\"]/div[5]/div[2]/div[{i}]/div/ul/li[3]/ul/li/dl/dd")]
+            sta_list += [station.text_content().replace("○", "") for station in fromstring(content).xpath(f"//*[@id=\"route01\"]/div[5]/div[2]/div[{i}]/ul/li[2]/ul/li/dl/dd")]
+
+    # 出発駅と到着駅を配列に追加
     sta_list.insert(0, from_station)
     sta_list.append(to_station)
 
-    #値段を取得
+    # 値段を取得（正規表現ver.）
     raw_price = fromstring(content).xpath(cpass)[0].text_content()
-    price_str = raw_price.split("：")[-1].replace("円", "")
-    price = int(price_str)
+    price_str = raw_price.split("：")[-1].replace(",", "")
+    price = (re.search('^\d+' , price_str)).group(0)
 
     return ResultData(sta_list, price)
 
-if __name__ == "__main__":
-    print(requests)
-    result = search("東京駅", "渋谷駅")
-    print(result.cost, result.stations)
+# 確認出力
+# result = search("東京", "渋谷")
+# print(result.stations)
+# print(result.cost)
